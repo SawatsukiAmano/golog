2e8a4f0587dd28e83bb50e10947b3a80b0a587f9
newreport <haruki959@qq.com>
2022-05-06 22:42:24 +0800

    .

---
+++ src/service/v1/Model/Blog.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Blog
+    {
+        /// <summary>
+        /// 文章id
+        /// </summary>
+        public int BlogId { get; set; }
+        /// <summary>
+        /// 用户名
+        /// </summary>
+        public string UserName { get; set; }
+        /// <summary>
+        /// 文章名
+        /// </summary>
+        public string BlogName { get; set; }
+        /// <summary>
+        /// 分类
+        /// </summary>
+        public string Category { get; set; }
+        /// <summary>
+        /// 第一次发布时间
+        /// </summary>
+        public DateTime CreatedTime { get; set; }
+        /// <summary>
+        /// 最后一次发布时间
+        /// </summary>
+        public DateTime LatestTime { get; set; }
+
+        public string CurrEditTxt { get; set; }
+    }
+}
