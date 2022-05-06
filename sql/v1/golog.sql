b69da83ed700b415014af058c4c568a3c8264fc8
newreport <haruki959@qq.com>
2022-05-06 22:30:07 +0800

    .

---
+++ sql/v1/golog.sql
@@ -0,0 +1,79 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : 开发服务器_MySQL
+ Source Server Type    : MySQL
+ Source Server Version : 80024
+ Source Host           : 42.193.14.174:3306
+ Source Schema         : golog
+
+ Target Server Type    : MySQL
+ Target Server Version : 80024
+ File Encoding         : 65001
+
+ Date: 06/05/2022 22:04:01
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for blog
+-- ----------------------------
+DROP TABLE IF EXISTS `blog`;
+CREATE TABLE `blog`  (
+  `BlogID` int(0) NOT NULL AUTO_INCREMENT,
+  `UserName` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '关联外键',
+  `Name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '文章名',
+  `Category` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '分组',
+  `CreateTime` datetime(0) NULL DEFAULT NULL COMMENT '创建时间',
+  `LatestTime` datetime(0) NULL DEFAULT NULL COMMENT '最后发布时间',
+  `CurrEditTxt` mediumtext CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL COMMENT '正在编辑的文本',
+  `ViewTxt` mediumtext CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL COMMENT '显示的 输出文章',
+  PRIMARY KEY (`BlogID`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for comment
+-- ----------------------------
+DROP TABLE IF EXISTS `comment`;
+CREATE TABLE `comment`  (
+  `CommentId` int(0) NOT NULL AUTO_INCREMENT,
+  `BlogId` int(0) NULL DEFAULT NULL COMMENT '文章ID',
+  `UserId` int(0) NULL DEFAULT NULL COMMENT '用户ID',
+  `CommentTime` datetime(0) NULL DEFAULT NULL COMMENT '评论时间',
+  `CommentText` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL COMMENT '评论内容 MK',
+  PRIMARY KEY (`CommentId`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for label
+-- ----------------------------
+DROP TABLE IF EXISTS `label`;
+CREATE TABLE `label`  (
+  `LabelId` int(0) NOT NULL AUTO_INCREMENT,
+  `LabelName` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '标签名',
+  `BlogId` int(0) NULL DEFAULT NULL COMMENT '文章Id',
+  `BlogName` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '文章名',
+  PRIMARY KEY (`LabelId`) USING BTREE,
+  UNIQUE INDEX `LabeName_Index`(`LabelName`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for user
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user`  (
+  `UserId` int(0) NOT NULL AUTO_INCREMENT,
+  `UserName` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '用户名',
+  `Password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '密码',
+  `NickName` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '昵称',
+  `Email` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '邮箱',
+  `VXId` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `GitId` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `QQId` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`UserId`) USING BTREE,
+  UNIQUE INDEX `UserName_Index`(`UserName`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+SET FOREIGN_KEY_CHECKS = 1;
---
+++ src/service/v1/API/Controllers/LoginController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+
+        [HttpGet]
+        public string GetTest()
+        {
+            return "连接成功" + DateTime.Now;
+        }
+    }
+}
